54c16d61d880b9a4458382fef6c1bc2c8390a4ad
sjwilczynski <sjwilczynski@gmail.com>
2018-09-02 21:48:36 +0200

    get rid of bash - refactor1

---
+++ source/R/sim_data_function.R
@@ -0,0 +1,20 @@
+require(varclust, quietly = TRUE)
+
+sim.data <- function(n, p, SNR, K, max.dim, mode, to.file=TRUE) {
+  if(mode == 0){
+    X <- data.simulation.factors(n = n, SNR = SNR, K = K, numb.vars = p/K,
+                                 numb.factors = K*max.dim/2,
+                                 min.dim = 2, max.dim = max.dim,
+                                 equal.dims = FALSE, separation.parameter = 0.2)$X
+  } else {
+    X <- data.simulation(n = n, SNR = SNR, K = K, numb.vars = p/K,
+                         min.dim = 2, max.dim = max.dim,
+                         equal.dims = FALSE)$X
+  }
+  
+  if(to.file){
+    file.name = paste("data/X_", max.dim, "_", SNR[1], ".csv", sep='')
+    write.table(X, file=file.name, row.names=F, col.names=F, sep=',')
+  }
+  X
+}
---
+++ source/R/sim_diff_snr.R
@@ -0,0 +1,28 @@
+require(varclust, quietly = TRUE)
+source("source/R/sim_data_function.R")
+
+args    <- commandArgs(trailingOnly = TRUE)
+n       <- as.numeric(args[1])
+p       <- as.numeric(args[2])
+SNR     <- as.numeric(args[3]) #Signal to Noise ratio
+K       <- as.numeric(args[4]) #number of subspaces
+max.dim <- as.numeric(args[5]) #maximal subspace dimension
+mode    <- as.numeric(args[6]) #shared/ not shared facors
+iter    <- as.numeric(args[7]) #number of iterations
+ratio   <- as.numeric(args[8]) #ration between subsequent SNRs
+
+set.seed(n + p + SNR + K)
+
+if (length(SNR) == 1) {
+  SNR <- 1:K * SNR
+}
+
+for (i in 1:iter) {
+  print(paste("Starting iteration", i))
+  X <- sim.data(n, p, SNR, K, max.dim, mode)
+  command <- paste0("\"cd('source/matlab'), simulations(", paste(n,max.dim,SNR[1],K,p,sep=","),"),quit()\"")
+  system2("matlab", args = c("-nosplash", "-nodisplay", "-nojvm", "-r", command), stdout = "tmpM")
+  #usuwanie tmpM i segmentation jak w bashowych, zapisywanie analizy pojedynczych ietracji normalnie do pliku, ale ogolnie przerobienie 
+  #na funkcje tak jak sim.data
+  
+}

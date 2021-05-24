d7501d8206198a6fbbd3c6f81019b49778630ea4
sjwilczynski <sjwilczynski@gmail.com>
2021-05-24 23:21:11 +0200

    fix for loop in new script

--- testiter.sh
+++ testiter.sh
@@ -9,11 +9,12 @@ snr=0.5
 maxdim=3
 numbruns=3
 maxiter=30
-i=0
+iters=(0)
 
 mkdir -p $name
 mkdir -p data/$name
 
+for i in ${iters[*]}
 do
     echo "Run $i start. Parameters: n=$n, p=${P[$i]}, snr=$snr, k=$k, maxdim=$maxdim, numbruns=$numbruns, maxiter=$maxiter"
     nohup Rscript source/R/sim_iter.R $n ${P[$i]} $snr $k $maxdim $numbruns $maxiter $name

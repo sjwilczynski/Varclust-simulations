ce9606690c10584d2db79fd69add0cd1961d1543
sjwilczynski <sjwilczynski@gmail.com>
2021-05-26 19:52:17 +0200

    update readme and output files

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ In order to run the simulations, you have to install *varclust* and all its depe
 
 ## Repository structure
 
-The main simulations scripts have names starting with numerical prefix. The results (including generated plots) are available in outputs_results folder. There, for example, *simulations4* directory corresponds to both partial and summarized results for *4_sim_max_dim.sh* script. *data* and *summaries* directories are temporary folders for generated datasets and results respectively. The R and matlab code invoking compared methods directly can be found in *source* directory.
+The main simulations scripts have names starting with numerical prefix. The results (including generated plots) are available in summaries folder. There, for example, *4_sim_max_dim* directory corresponds to both partial and summarized results for *4_sim_max_dim.sh* script. *data* is a temporary folder for generated datasets. The R and matlab code invoking compared methods directly can be found in *source* directory.
 
 ## Running simulations
 
@@ -24,3 +24,25 @@ However, due to the complexity of the methods, we recommend to run all main simu
 ```bash
 ./all_simulations.sh 4
 ```
+
+Additionally, due to the time needed to run each of the scripts, to prevent them from being interupted due to e.g disconnects, using [screen](https://linuxize.com/post/how-to-use-linux-screen/) command is recommended.
+
+## Step by step instruction for running simulations 
+
+1. Checkout this repository
+1. Install required packages by running `Rscript packages/packages.R`
+1. Type `screen -S simulation_1` to start the new virtual terminal session
+1. Run `./all_simulations.sh 1` to start first simulation script
+1. Detach from the screen session using `Ctrl+a+d` shortcut
+1. Similarly run other simulation scripts on different machines
+1. After few days log back to the machine and reattach screen session using `screen -r` command
+1. Check if it's completed. If there was an issue:
+    1. Restart the simulation
+    1. If you think it's a bug (you can confirm this by looking at the relevant `nohup.out` file) report issue
+
+**TIP** for running simulations in the Math department of Univesity of Wroclaw:
+Login to chaos and then run each of the simulations on machines from win01 to win12
+
+## Troubleshooting
+
+1. I can't execute the script becuase of "No access" or similar error - make sure to run `chmod u+x script_name.sh`
--- all_simulations.sh
+++ all_simulations.sh
@@ -5,58 +5,58 @@ to_run="$1" #name of the simultaion
 case $to_run in
     "1" )
         echo "mlcc.reps with twice overestimated maximal dimension of subspace"
-        nohup ./1_sim_2max_dim.sh | tee nohup.out 
+        nohup ./1_sim_2max_dim.sh | tee 1-nohup.out 
         ;;
     "2" )
         echo "mlcc.bic for not shared mode"
-        nohup ./2_sim_detect_number_of_clusters_mode_not_shared.sh | tee nohup.out 
+        nohup ./2_sim_detect_number_of_clusters_mode_not_shared.sh | tee 2-nohup.out 
         ;;
     "3" )
         echo "mlcc.bic for shared mode"
-        nohup ./3_sim_detect_number_of_clusters_mode_shared.sh | tee nohup.out 
+        nohup ./3_sim_detect_number_of_clusters_mode_shared.sh | tee 3-nohup.out 
         ;;
     "4" )
         echo "mlcc.reps with increasing subspace dimension"
-        nohup ./4_sim_max_dim.sh | tee nohup.out 
+        nohup ./4_sim_max_dim.sh | tee 4-nohup.out 
         ;;
     "5" )
         echo "mlcc.reps with respect to mode"
-        nohup ./5_sim_mode.sh | tee nohup.out 
+        nohup ./5_sim_mode.sh | tee 5-nohup.out 
         ;;
     "6" )
         echo "mlcc.reps with increasing number of clusters"
-        nohup ./6_sim_number_of_clusters.sh | tee nohup.out 
+        nohup ./6_sim_number_of_clusters.sh | tee 6-nohup.out 
         ;;
     "7" )
         echo "mlcc.reps with increasing number of variables"
-        nohup ./7_sim_number_of_vars.sh | tee nohup.out 
+        nohup ./7_sim_number_of_vars.sh | tee 7-nohup.out 
         ;;
     "8" )
         echo "mlcc.reps with increasing snr, mode not shared"
-        nohup ./8_sim_snr_mode_not_shared.sh | tee nohup.out 
+        nohup ./8_sim_snr_mode_not_shared.sh | tee 8-nohup.out 
         ;;
     "9" )
         echo "mlcc.reps with increasing snr, mode shared"
-        nohup ./9_sim_snr_mode_shared.sh | tee nohup.out 
+        nohup ./9_sim_snr_mode_shared.sh | tee 9-nohup.out 
         ;;
     "10" )
         echo "execution time with repsect to the number of variables"
-        nohup ./10_sim_time_variables.sh | tee nohup.out 
+        nohup ./10_sim_time_variables.sh | tee 10-nohup.out 
         ;;
     "11" )
         echo "execution time with repsect to the number of clusters"
-        nohup ./11_sim_time_clusters.sh | tee nohup.out 
+        nohup ./11_sim_time_clusters.sh | tee 11-nohup.out 
         ;;
     "12" )
         echo "mBIC with repsect to the number of iterations"
-        nohup ./12_sim_iter.sh | tee nohup.out 
+        nohup ./12_sim_iter.sh | tee 12-nohup.out 
         ;;
     "test" )
         echo "test simulations"
-        nohup ./testbicsim.sh | tee nohup.out
-        nohup ./testsim.sh | tee nohup.out
-        nohup ./testtimesim.sh | tee nohup.out 
-        nohup ./testiter.sh | tee nohup.out 
+        nohup ./testbicsim.sh | tee test-nohup.out
+        nohup ./testsim.sh | tee test-nohup.out
+        nohup ./testtimesim.sh | tee test-nohup.out 
+        nohup ./testiter.sh | tee test-nohup.out 
         ;;
     "all" )
         echo "Run all simulations"

7ccad1a2123bde454b683956c02e697e5905f203
sjwilczynski <sjwilczynski@gmail.com>
2021-05-26 18:57:44 +0200

    move results around

--- additional_scripts/delete_sim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {1..11}
-do
-    rm -r simulations$i/
-done
-
--- additional_scripts/get_results.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1..11}
-do
-    mkdir -p outputsResults/simulations$i
-    cp -r simulations$i/summaries/*   outputsResults/simulations$i/
-done
-
